2a5ebaddbfc613ad22bfab3729b3a401f94dca10
n2o <cmeter@googlemail.com>
2014-03-14 21:41:25 +0100

    Added AuthComponent to login into an admin menu

--- app/routes.php
+++ app/routes.php
@@ -24,3 +24,13 @@ Route::get('users', function() {
 
 	return View::make('master')->with('users', $users);
 });
+
+Route::get('admin/logout', array('as' => 'admin.logout', 'uses'     => 'App\Controllers\Admin\AuthController@getLogout'));
+Route::get('admin/login',  array('as' => 'admin.login', 'uses'      => 'App\Controllers\Admin\AuthController@getLogin'));
+Route::post('admin/login', array('as' => 'admin.login.post', 'uses' => 'App\Controllers\Admin\AuthController@postLogin'));
+
+Route::group(array('prefix' => 'admin', 'before' => 'auth.admin'), function() {
+	Route::any('/', 'App\Controllers\Admin\PagesController@index');
+	Route::resource('articles', 'App\Controllers\Admin\ArticlesController');
+	Route::resource('pages', 'App\Controllers\Admin\PagesController');
+});
